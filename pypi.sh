99a5b3cb9de6090eaaf5485ecef6d4ab4f1f7f13
buffet <saltman21@naver.com>
2024-04-03 21:32:57 +0900

    korean nltk tips

--- pypi.sh
+++
@@ -1,8 +0,0 @@
-folder_info='pytip.egg-info'
-
-source /home/buffet/Coding/venv/Py/bin/activate
-python setup.py bdist_wheel
-python3.10 -m twine upload dist/*
-mv ./dist/*.whl ./
-rm -rf dist build "$folder_info"
-deactivate
